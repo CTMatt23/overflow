2728bfe628510c289b0724a38c7953f8a70f72c2
CTMatt23 <mastell@my.ccsu.edu>
2022-02-08 19:08:30 -0500

    add maps

---
+++ Stuffing/stuffed/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+import Header from "./Header";
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
---
+++ Stuffing/stuffed/src/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Button from "@mui/material/Button";
+const Header = () => {
+  return (
+    <header>
+      Stuffed
+      <Button variant="contained">Hello World</Button>
+    </header>
+  );
+};
+export default Header;
